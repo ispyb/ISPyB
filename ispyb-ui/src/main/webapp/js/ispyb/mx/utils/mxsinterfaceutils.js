ef8b0104d72090d9fc77a39fd7bc9136e6efee32
Solange Delageniere <solange.delageniere@esrf.fr>
2015-06-10 13:10:18 +0000

    javascript refactoring
    svn path=/trunk/ispyb-parent/; revision=25

--- ispyb-ui/src/main/webapp/js/ispyb/mx/utils/mxsinterfaceutils.js
+++ ispyb-ui/src/main/webapp/js/ispyb/mx/utils/mxsinterfaceutils.js
@@ -35,7 +35,7 @@ var MXUI = {
 	getGraphByPath : function(path) {
 
 		return "<div style='white-space:normal !important;word-wrap: break-word;'>" + 
-		"<a  target='_blank' href='viewResults.do?reqCode=viewJpegImageFromFile&file=" + 
+		"<a  target='_blank' href='viewResults.do?reqCode=getJpegImageFromFile&file=" + 
 		path + "'  styleClass='LIST'>" + 
 		"<img width='120' height='100' src='imageDownload.do?reqCode=getImageJpgFromFile&file=" + 
 		path + "' border='0' alt='Click to zoom the image' >" + "</a></div>";
