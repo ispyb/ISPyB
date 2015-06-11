c8f2312ab2d4278a3262fe85a4088e8242553255
Solange Delageniere <solange.delageniere@esrf.fr>
2015-06-11 06:37:08 +0000

    svn path=/trunk/ispyb-parent/; revision=39

--- ispyb-ui/src/main/webapp/js/ispyb/min/ispyb-mx.js
+++ ispyb-ui/src/main/webapp/js/ispyb/min/ispyb-mx.js
@@ -12174,14 +12174,8 @@ SessionViewGrid.prototype._getColumns = function () {
 			if (record.data.imageThumbnailPath.search("noDiffractionThumbnail.jpg") != -1) {
 				return "";
 			}
-			return "<div style='white-space:normal !important;word-wrap: break-word;'>" + 
-					"<a  href='viewResults.do?reqCode=viewJpegImage&imageId=" + 
-					record.data.imageId + 
-					"'  styleClass='LIST'>" + 
-					"<img width='110' height='110' src='imageDownload.do?reqCode=getImageJpgFromFile&file=" + 
-					record.data.imageThumbnailPath + 
-					"' border='0' alt='Click to zoom the image' >" + 
-					"</a></div>";
+			return MXUI.getJpegByImageIdandPath(record.data.imageId,record.data.imageThumbnailPath );
+			
 		}
 	}
 	
@@ -12198,14 +12192,7 @@ SessionViewGrid.prototype._getColumns = function () {
 			if (record.data.crystalSnapshotPath.search("noXtalThumbnail.jpg") != -1) {
 				return "";
 			}
-			return "<div style='white-space:normal !important;word-wrap: break-word;'>" + 
-					"<a  href='viewResults.do?reqCode=viewJpegImageFromFile&file=" + 
-					record.data.crystalSnapshotPath + 
-					"'  styleClass='LIST'>" + 
-					"<img width='110' height='83' src='imageDownload.do?reqCode=getImageJpgFromFile&file=" + 
-					record.data.crystalSnapshotPath + 
-					"' border='0' alt='Click to zoom the image' >" + 
-					"</a></div>";
+			return MXUI.getSnapshotByPath(record.data.crystalSnapshotPath);
 		}
 	}
 	
@@ -12311,14 +12298,8 @@ SessionViewGrid.prototype._getColumns = function () {
 				return "";
 			}
 		} else { // graph image
-			return "<div style='white-space:normal !important;word-wrap: break-word;'>" + 
-					"<a  href='viewResults.do?reqCode=viewJpegImageFromFile&file=" + 
-					record.data.graphPath + 
-					"'  styleClass='LIST'>" + 
-					"<img width='120' height='100' src='imageDownload.do?reqCode=getImageJpgFromFile&file=" + 
-					record.data.graphPath + 
-					"' border='0' alt='Click to zoom the image' >" + 
-					"</a></div>";
+			return MXUI.getGraphByPath(record.data.graphPath);
+			
 		}
 	}
 	
@@ -12331,14 +12312,7 @@ SessionViewGrid.prototype._getColumns = function () {
 				return "";
 			}
 		} else {
-			return "<div style='white-space:normal !important;word-wrap: break-word;'>" + 
-					"<a  href='viewResults.do?reqCode=viewJpegImageFromFile&file=" + 
-					record.data.graph2Path + 
-					"'  styleClass='LIST'>" + 
-					"<img width='120' height='100' src='imageDownload.do?reqCode=getImageJpgFromFile&file=" + 
-					record.data.graph2Path + 
-					"' border='0' alt='Click to zoom the image' >" + 
-					"</a></div>";
+			return MXUI.getGraphByPath(record.data.graph2Path);
 		}
 	}
 	
@@ -22817,3 +22791,53 @@ Shipping.prototype.pastePuck = function (dewarId) {
 	}
 	return null;
 };
+var MXUI = {
+	//interval : 60000,
+		interval : 40000,
+
+	getShippingURL : function(shippingId) {
+		return '/ispyb/user/viewProjectList.do?reqCode=display&menu=shipment&shippingId=' + shippingId;
+	},
+	
+	getJpegByImageIdandPath : function(imageId, path) {
+		return "<div style='white-space:normal !important;word-wrap: break-word;'>" + 
+		"<a  target='_blank' href='viewResults.do?reqCode=viewJpegImage&imageId=" + 
+		imageId + 
+		"'  styleClass='LIST' >" + 
+		"<img width='110' height='110' src='imageDownload.do?reqCode=getImageJpgFromFile&file=" + 
+		path + 
+		"' border='0' alt='Click to zoom the image'>" + 
+		"</a></div>";
+	},
+	
+	getSnapshotByPath : function(path) {
+		return "<div style='white-space:normal !important;word-wrap: break-word;'>" + 
+		"<a  target='_blank' href='viewResults.do?reqCode=getJpegImageFromFile&file=" + 
+		path + 
+		"'  styleClass='LIST'>" + 
+		"<img width='110' height='83' src='imageDownload.do?reqCode=getImageJpgFromFile&file=" + 
+		path + 
+		"' border='0' alt='Click to zoom the image' >" + 
+		"</a></div>";
+		
+	//	var url = "viewResults.do?reqCode=getJpegImageFromFile&file=" + path;
+	//	var event = "OnClick= window.open('" + url + "')";
+	//	return '<img src=' + url + '   height="83" width="110" border="0" alt="Click to zoom the image'  + event + '>';
+	},
+	
+	getGraphByPath : function(path) {
+
+		return "<div style='white-space:normal !important;word-wrap: break-word;'>" + 
+		"<a  target='_blank' href='viewResults.do?reqCode=getJpegImageFromFile&file=" + 
+		path + "'  styleClass='LIST'>" + 
+		"<img width='120' height='100' src='imageDownload.do?reqCode=getImageJpgFromFile&file=" + 
+		path + "' border='0' alt='Click to zoom the image' >" + "</a></div>";
+	},
+
+	getURL : function() {
+		return this.getBaseURL() + '?reqCode=getImage';
+
+	},
+
+};
+
