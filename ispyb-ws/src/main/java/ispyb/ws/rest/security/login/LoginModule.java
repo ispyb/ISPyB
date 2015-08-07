a1d76ee917e5211b5368412cb68e765cc88514aa
Alejandro de Maria <demariaa@esrf.fr>
2015-08-07 15:41:47 +0000

    LDAP user validation gets now the roles of the user
    svn path=/trunk/ispyb-parent/; revision=121

--- ispyb-ws/src/main/java/ispyb/ws/rest/security/login/LoginModule.java
+++ ispyb-ws/src/main/java/ispyb/ws/rest/security/login/LoginModule.java
@@ -1,4 +1,6 @@
-package ispyb.ws.security.login;
+package ispyb.ws.rest.security.login;
+
+import ispyb.server.security.EmployeeVO;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,61 +13,71 @@ import javax.naming.directory.Attributes;
 import javax.naming.directory.SearchControls;
 import javax.naming.directory.SearchResult;
 import javax.naming.ldap.InitialLdapContext;
+import javax.naming.ldap.LdapContext;
 
 public class LoginModule {
+		static String groupUniqueMemberName = "uniqueMember";
+		static String principalDNSuffix = ",ou=People,dc=esrf,dc=fr";
+		static String groupCtxDN = "ou=Pxwebgroups,dc=esrf,dc=fr";
+		static String principalDNPrefix = "uid=";
+		static String groupAttributeID = "cn";
+		static String server = "ldap://ldap.esrf.fr:389/";
 
-    public static List<String> authenticate(String username, String password) throws NamingException{
-	List<String> myRoles = new ArrayList<String>();
-	String groupUniqueMemberName = "uniqueMember";
-	String principalDNSuffix = ",ou=People,dc=esrf,dc=fr";
-	String groupCtxDN = "ou=Pxwebgroups,dc=esrf,dc=fr";
-	String principalDNPrefix = "uid=";
-	String groupAttributeID = "cn";
 
-	Properties env = new Properties();
-	env.put("java.naming.factory.initial", "com.sun.jndi.ldap.LdapCtxFactory");
-	env.put("principalDNPrefix", principalDNPrefix);
-	env.put("java.naming.security.principal", "uid=" + username + ",ou=People,dc=esrf,dc=fr");
-	env.put("groupAttributeID", groupAttributeID);
-	env.put("groupCtxDN", groupCtxDN);
-	env.put("principalDNSuffix", principalDNSuffix);
-
-	env.put("allowEmptyPasswords", "false");
-	env.put("groupUniqueMember", groupUniqueMemberName);
-	env.put("jboss.security.security_domain", "ispyb");
-	env.put("java.naming.provider.url", "ldap://ldap.esrf.fr:389/");
-	env.put("java.naming.security.authentication", "simple");
-	env.put("java.naming.security.credentials", password);
-
-	String userDN = principalDNPrefix + username + principalDNSuffix;
-
-	InitialLdapContext ctx = new InitialLdapContext(env, null);
-
-	String filter = new StringBuffer().append("(&").append("(objectClass=groupOfUniqueNames)")
-		.append("(" + groupUniqueMemberName + "=").append(userDN).append(")").append(")").toString();
-	// Set up search constraints
-	SearchControls cons = new SearchControls();
-	cons.setSearchScope(SearchControls.SUBTREE_SCOPE);
-	// Search
-	NamingEnumeration<SearchResult> answer = ctx.search(groupCtxDN, filter, cons);
-	while (answer.hasMore()) {
-	    SearchResult sr = answer.next();
-	    Attributes attrs = sr.getAttributes();
-
-	    Attribute roles = attrs.get(groupAttributeID);
-	    for (int r = 0; r < roles.size(); r++) {
-		Object value = roles.get(r);
-		String roleName = null;
-		roleName = value.toString();
-		// fill roles array
-		if (roleName != null) {
-		    myRoles.add(roleName);
-		}
-
-	    }
+	public static Properties getConnectionProperties(String username, String password){
+		Properties env = new Properties();
+		env.put("java.naming.factory.initial", "com.sun.jndi.ldap.LdapCtxFactory");
+		env.put("principalDNPrefix", principalDNPrefix);
+		env.put("java.naming.security.principal", "uid=" + username + ",ou=People,dc=esrf,dc=fr");
+		env.put("groupAttributeID", groupAttributeID);
+		env.put("groupCtxDN", groupCtxDN);
+		env.put("principalDNSuffix", principalDNSuffix);
+		env.put("allowEmptyPasswords", "false");
+		env.put("groupUniqueMember", groupUniqueMemberName);
+		env.put("jboss.security.security_domain", "ispyb");
+		env.put("java.naming.provider.url", server);
+		env.put("java.naming.security.authentication", "simple");
+		env.put("java.naming.security.credentials", password);
+		return env;
+		
 	}
-	return myRoles;
 	
+	public static String getFilter(String username){
+		String userDN = principalDNPrefix + username + principalDNSuffix;
+		return new StringBuffer().append("(&")
+				.append("(objectClass=groupOfUniqueNames)")
+				.append("(" + groupUniqueMemberName + "=").append(userDN)
+				.append(")").append(")").toString();
+	}
 	
-    }
+	public static List<String> authenticate(String username, String password)
+			throws NamingException {
+		
+		List<String> myRoles = new ArrayList<String>();
+		
+		InitialLdapContext ctx = new InitialLdapContext(getConnectionProperties(username, password), null);
+		
+		// Set up search constraints
+		SearchControls cons = new SearchControls();
+		cons.setSearchScope(SearchControls.SUBTREE_SCOPE);
+		// Search
+		NamingEnumeration<SearchResult> answer = ctx.search(groupCtxDN, getFilter(username),cons);
+		while (answer.hasMore()) {
+			SearchResult sr = answer.next();
+			Attributes attrs = sr.getAttributes();
+			System.out.println(attrs.toString());
+			Attribute roles = attrs.get(groupAttributeID);
+			for (int r = 0; r < roles.size(); r++) {
+				Object value = roles.get(r);
+				String roleName = null;
+				roleName = value.toString();
+				// fill roles array
+				if (roleName != null) {
+					myRoles.add(roleName);
+				}
+
+			}
+		}
+		return myRoles;
+	}
 }
