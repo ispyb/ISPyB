f2c893dfb6a1ce04c9d5b6cda544fbe8ff46d4b2
Alejandro de Maria <demariaa@esrf.fr>
2015-08-07 15:42:23 +0000

    If a user is authenticated then the token is written on the database
    svn path=/trunk/ispyb-parent/; revision=122

--- ispyb-ws/src/main/java/ispyb/ws/rest/security/AuthenticationRestWebService.java
+++ ispyb-ws/src/main/java/ispyb/ws/rest/security/AuthenticationRestWebService.java
@@ -1,68 +1,90 @@
 package ispyb.ws.rest.security;
 
+import ispyb.server.common.vos.login.Login3VO;
 import ispyb.ws.rest.RestWebService;
-import ispyb.ws.security.login.LoginModule;
+import ispyb.ws.rest.saxs.BufferRestWebService;
+import ispyb.ws.rest.security.login.LoginModule;
 
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Formatter;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Properties;
+import java.util.UUID;
 
 import javax.annotation.security.PermitAll;
-import javax.naming.NamingEnumeration;
-import javax.naming.NamingException;
-import javax.naming.directory.Attribute;
-import javax.naming.directory.Attributes;
-import javax.naming.directory.SearchControls;
-import javax.naming.directory.SearchResult;
-import javax.naming.ldap.InitialLdapContext;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
+import org.apache.log4j.Logger;
+
 @Path("/")
 public class AuthenticationRestWebService extends RestWebService {
-
-    @PermitAll
-    @POST
-    @Path("/authenticate")
-    @Produces({ "application/json" })
-    public Response list(@FormParam("login") String login, @FormParam("password") String password) throws Exception {
-	String sha1 = null;
-	try {
-	    MessageDigest crypt = MessageDigest.getInstance("SHA-1");
-	    crypt.reset();
-	    crypt.update(password.getBytes("UTF-8"));
-	    sha1 = byteToHex(crypt.digest());
-
-	    List<String> roles = LoginModule.authenticate(login, password);
-	    HashMap<String, Object> cookie = new HashMap<String, Object>();
-	    cookie.put("cookie", sha1);
-	    cookie.put("roles", roles);
-	    return sendResponse(cookie);
-	} catch (NoSuchAlgorithmException e) {
-	    e.printStackTrace();
-	} catch (UnsupportedEncodingException e) {
-	    e.printStackTrace();
+	private final static Logger logger = Logger.getLogger(AuthenticationRestWebService.class);
+	
+	private String generateRamdomUUID() throws NoSuchAlgorithmException, UnsupportedEncodingException{
+		MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+		crypt.reset();
+		crypt.update( UUID.randomUUID().toString().getBytes("UTF-8"));
+		return byteToHex(crypt.digest());
 	}
-	return unauthorizedResponse();
-    }
-
-    private static String byteToHex(final byte[] hash) {
-	Formatter formatter = new Formatter();
-	for (byte b : hash) {
-	    formatter.format("%02x", b);
+	
+	@PermitAll
+	@POST
+	@Path("/authenticate")
+	@Produces({ "application/json" })
+	public Response authenticate(@FormParam("login") String login, @FormParam("password") String password) throws Exception {
+		String methodName = "authenticate";
+		long id = this.logInit(methodName, logger, login);
+		try {
+			
+//			List<String> roles = LoginModule.authenticate(login, password);
+			List<String> roles = new ArrayList<String>();
+			
+			roles.add("User");
+			
+			String token = generateRamdomUUID();
+			
+			HashMap<String, Object> cookie = new HashMap<String, Object>();
+			cookie.put("token", token);
+			cookie.put("roles", roles);
+			
+			/** Creating the login token on database **/
+			Login3VO login3vo = new Login3VO();
+			login3vo.setToken(token);
+			login3vo.setUsername(login);
+			login3vo.setRoles(roles.toString());
+			
+			/** Calculating expiration time **/
+			Calendar calendar = Calendar.getInstance(); // gets a calendar using the default time zone and locale.
+			calendar.add(Calendar.HOUR, 3);
+			Date expirationTime = calendar.getTime();
+			
+			login3vo.setExpirationTime(expirationTime);
+			System.out.println(this.getGson().toJson(login3vo));
+			/** Saving login on database **/
+			this.getLogin3Service().persist(login3vo);
+			return sendResponse(cookie);
+		} catch (Exception e) {
+			return this.logError(methodName, e, id, logger);
+		}
 	}
-	String result = formatter.toString();
-	formatter.close();
-	return result;
-    }
 
-    
+	private static String byteToHex(final byte[] hash) {
+		Formatter formatter = new Formatter();
+		for (byte b : hash) {
+			formatter.format("%02x", b);
+		}
+		String result = formatter.toString();
+		formatter.close();
+		return result;
+	}
+
 }
