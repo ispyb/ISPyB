6ff8a66557db48e964ba8691a5dbd3b119b2e2db
Alejandro de Maria <demariaa@esrf.fr>
2015-08-19 16:57:10 +0000

    Added the jars for the maven local repository as well as an script to make installation easier
    svn path=/trunk/ispyb-parent/; revision=134

---
+++ configuration/maven/installCustomJavaLibrariesToMaven.sh
@@ -0,0 +1,8 @@
+mvn install:install-file -Dfile=jar/securityfilter.jar -DgroupId=securityfilter -DartifactId=securityfilter -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=jar/securityaes.jar -DgroupId=securityaes -DartifactId=securityaes -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=jar/jhdf.jar -DgroupId=jhdf -DartifactId=jhdf -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=jar/jhdf5.jar -DgroupId=jhdf5 -DartifactId=jhdf5 -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=jar/jhdf5obj.jar -DgroupId=jhdf5obj -DartifactId=jhdf5obj -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=jar/jhdfobj.jar -DgroupId=jhdfobj -DartifactId=jhdfobj -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=jar/Struts-Layout-1.2.jar -DgroupId=struts-layout -DartifactId=struts-layout -Dversion=1.2 -Dpackaging=jar
+
