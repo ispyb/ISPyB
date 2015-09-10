1e2595f27b378d32c0baf1569f677fcfd5021947
Alejandro de Maria <demariaa@esrf.fr>
2015-09-10 08:28:37 +0000

    Added Stats3Service to provide statistics about number of experiments done, macromolecules collected, frames, etc...
    svn path=/trunk/ispyb-parent/; revision=197

---
+++ ispyb-ejb/src/main/java/ispyb/server/biosaxs/services/stats/Stats3ServiceBean.java
@@ -0,0 +1,94 @@
+/*******************************************************************************
+ * This file is part of ISPyB.
+ * 
+ * ISPyB is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ISPyB is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ISPyB.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors : S. Delageniere, R. Leal, L. Launer, K. Levik, S. Veyrier, P. Brenchereau, M. Bodin, A. De Maria Antolinos
+ ******************************************************************************************************************************/
+
+package ispyb.server.biosaxs.services.stats;
+
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Map;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.transform.AliasToEntityMapResultTransformer;
+
+
+
+@Stateless
+public class Stats3ServiceBean implements Stats3Service, Stats3ServiceLocal {
+	
+	private String GET_EXPERIMENT_COUNT_BY_DATE = " select count(*) from Experiment "
+			+ "										where experimentType = :TYPE and creationDate > :START and creationDate < :END";
+	
+	private String GET_FRAMES_COUNT_BY_DATE = " select count(*) from Frame "
+			+ "										where creationDate > :START and creationDate < :END";
+	
+	
+	private String GET_SAMPLES_COUNT_BY_DATE = "select count(*) from Specimen spe, Experiment exp where exp.experimentId = spe.experimentId and exp.creationDate > :START and exp.creationDate < :END";
+	
+	private String GET_SESSIONS = "select count(distinct(sessionId)) from Experiment where  creationDate > :START and creationDate < :END";
+	
+	@PersistenceContext(unitName = "ispyb_db")
+	private EntityManager entityManager;
+
+
+	@Override
+	public List getSamplesBy(String start, String end) {
+		Session session = (Session) this.entityManager.getDelegate();
+		SQLQuery query = session.createSQLQuery(GET_SAMPLES_COUNT_BY_DATE);
+		query.setParameter("START", start);
+		query.setParameter("END", end);
+		return 	query.list();
+	}
+	
+	@Override
+	public List getSessionsBy(String start, String end) {
+		Session session = (Session) this.entityManager.getDelegate();
+		SQLQuery query = session.createSQLQuery(GET_SESSIONS);
+		query.setParameter("START", start);
+		query.setParameter("END", end);
+		return 	query.list();
+	}
+	
+	@Override
+	public List getExperimentsBy(String type, String start, String end) {
+		Session session = (Session) this.entityManager.getDelegate();
+		SQLQuery query = session.createSQLQuery(GET_EXPERIMENT_COUNT_BY_DATE);
+		query.setParameter("TYPE", type);
+		query.setParameter("START", start);
+		query.setParameter("END", end);
+		return 	query.list();
+	}
+	
+	@Override
+	public List getFramesBy( String start, String end) {
+		Session session = (Session) this.entityManager.getDelegate();
+		SQLQuery query = session.createSQLQuery(GET_FRAMES_COUNT_BY_DATE);
+		query.setParameter("START", start);
+		query.setParameter("END", end);
+		return 	query.list();
+	}
+		
+}
