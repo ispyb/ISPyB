c0da200cdb8f61b66ea198f0bc15ce5425e0a313
Alejandro de Maria <demariaa@esrf.fr>
2015-09-14 08:30:00 +0000

    Test of a unit test
    svn path=/trunk/ispyb-parent/; revision=201

---
+++ ispyb-ejb/src/test/java/ispyb/saxs/test/reader/FileReader.java
@@ -0,0 +1,19 @@
+package ispyb.saxs.test.reader;
+
+import ispyb.server.biosaxs.services.utils.reader.dat.DatFile;
+import ispyb.server.biosaxs.services.utils.reader.dat.FactoryProducer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class FileReader {
+
+	
+	@Test
+	public void test(){
+		System.out.println("------------>  This is a test");
+
+	}
+}
