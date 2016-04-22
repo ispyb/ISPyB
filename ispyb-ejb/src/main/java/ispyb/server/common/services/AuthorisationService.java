955be92fa4cae481536b23baf7b5639e7f8c59fd
Solange Delageniere <solange.delageniere@esrf.fr>
2016-04-22 09:11:20 +0000

    new Servive bean to mange authorisation
    svn path=/trunk/ispyb-parent/; revision=475

---
+++ ispyb-ejb/src/main/java/ispyb/server/common/services/AuthorisationService.java
@@ -0,0 +1,53 @@
+/*************************************************************************************************
+ * This file is part of ISPyB.
+ * 
+ * ISPyB is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ISPyB is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ISPyB.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors : S. Delageniere, R. Leal, L. Launer, K. Levik, S. Veyrier, P. Brenchereau, M. Bodin, A. De Maria Antolinos
+ ****************************************************************************************************/
+
+package ispyb.server.common.services;
+
+import javax.ejb.Remote;
+
+import ispyb.server.common.exceptions.AccessDeniedException;
+import ispyb.server.mx.vos.collections.Session3VO;
+
+@Remote
+public interface AuthorisationService {
+
+	/**
+	 * 
+	 * @param sessionPk
+	 * @param proposalPk
+	 * @return
+	 * @throws AccessDeniedException
+	 *             if the user has no access right
+	 * @throws Exception
+	 */
+	 public void checkUserRightToAccessSession(Integer sessionPk )
+			throws AccessDeniedException, Exception;
+	
+	/**
+	 * 
+	 * @param sessionVO
+	 * @return
+	 * @throws AccessDeniedException
+	 *             if the user has no access right
+	 * @throws Exception
+	 */
+	public void checkUserRightToAccessSession( Session3VO vo )
+			throws AccessDeniedException, Exception;
+
+}
---
+++ ispyb-ejb/src/main/java/ispyb/server/common/services/AuthorisationServiceBean.java
@@ -0,0 +1,111 @@
+/*************************************************************************************************
+ * This file is part of ISPyB.
+ * 
+ * ISPyB is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ISPyB is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ISPyB.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors : S. Delageniere, R. Leal, L. Launer, K. Levik, S. Veyrier, P. Brenchereau, M. Bodin, A. De Maria Antolinos
+ ****************************************************************************************************/
+package ispyb.server.common.services;
+
+import java.nio.file.AccessDeniedException;
+
+import javax.annotation.Resource;
+import javax.annotation.security.PermitAll;
+import javax.ejb.EJB;
+import javax.ejb.SessionContext;
+import javax.ejb.Stateless;
+
+import org.apache.log4j.Logger;
+
+import ispyb.server.common.services.sessions.Session3ServiceLocal;
+import ispyb.server.mx.vos.collections.Session3VO;
+
+
+/**
+ * <p>
+ * This session bean handles authorisation access.
+ * </p>
+ */
+@Stateless
+public class AuthorisationServiceBean implements AuthorisationService, AuthorisationServiceLocal {
+
+	private final static Logger LOG = Logger.getLogger(AuthorisationServiceBean.class);
+
+	@Resource
+	private SessionContext context;
+	
+	@EJB
+	private Session3ServiceLocal sessionService;
+
+
+	/**
+	 * 
+	 * @param proposalPk
+	 * @param sessionPk
+	 * @return
+	 * @throws AccessDeniedException
+	 *             if the user has no access right
+	 * @throws Exception
+	 */
+	public void checkUserRightToAccessSession(Integer sessionPk )
+			throws Exception{
+		
+		Session3VO vo = sessionService.findByPk(sessionPk, false, false, false);
+		Integer userPk = this.getUserPk();
+		if (vo == null) throw new Exception("no session has been found with this id");
+		if (! vo.getProposalVOId().equals(userPk)){
+			throw new Exception("Access not authorised to session:" + sessionPk + " for user:"+ userPk);
+		}
+		
+		return;
+	}
+	
+	/**
+	 * 
+	 * @param sessionVO
+	 * @return
+	 * @throws AccessDeniedException
+	 *             if the user has no access right
+	 * @throws Exception
+	 */
+	public void checkUserRightToAccessSession( Session3VO vo )
+			throws Exception{
+		Integer userPk = this.getUserPk();
+
+		
+		if (vo == null) throw new Exception("no session has been found");
+		if (! vo.getProposalVOId().equals(userPk)){
+			throw new Exception("Access not authorised to session:" + vo.toString() + " for user:"+ userPk);
+		}
+		
+		return;
+	}
+	
+	@PermitAll
+	// needed e.g. for unlocking BAG report when session ends
+	public Integer getUserPk() throws Exception {
+		Integer userPk = new Integer(0);
+		try {
+			userPk = Integer.valueOf(this.context.getCallerPrincipal().getName());
+		} catch (NumberFormatException e) {
+			if (LOG.isDebugEnabled()) {
+				LOG.debug("getUserPk: context.getCallerPrincipal=" + this.context.getCallerPrincipal().getName()
+						+ " can NOT be parsed as primary key!");
+			}
+		}
+		return userPk;
+	}
+	
+
+}
