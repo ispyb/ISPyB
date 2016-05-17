219104add3925dea839c83a7b25eaefd467b389e
Solange Delageniere <solange.delageniere@esrf.fr>
2016-05-17 13:43:39 +0000

    DB merging with DLS
    svn path=/trunk/ispyb-parent/; revision=508

---
+++ ispyb-ejb/sql_scripts/mysql/update_scripts/2016_05_17_DBMerging.sql
@@ -0,0 +1,13 @@
+use pydb;
+
+DROP TABLE `BLSample_has_EnergyScan`;
+
+DROP TABLE `Crystal_has_UUID`;
+
+DROP TABLE `DatamatrixInSampleChanger`;
+
+DROP TABLE `UntrustedRegion`;
+
+DROP TABLE `WorkflowDehydration`;
+
+DROP TABLE `Log4Stat`;
