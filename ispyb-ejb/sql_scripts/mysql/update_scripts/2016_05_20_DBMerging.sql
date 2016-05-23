8d533b5011e35e3e35aee07d84116773ecbbff40
Solange Delageniere <solange.delageniere@esrf.fr>
2016-05-23 14:58:12 +0000

    DB merging with DLS
    svn path=/trunk/ispyb-parent/; revision=520

--- ispyb-ejb/sql_scripts/mysql/update_scripts/2016_05_20_DBMerging.sql
+++ ispyb-ejb/sql_scripts/mysql/update_scripts/2016_05_20_DBMerging.sql
@@ -1,3 +1,15 @@
 use pydb;
 
 alter table Person drop passwd;
+
+SET foreign_key_checks = 0;
+
+DROP TABLE `WorkflowMesh`;
+DROP TABLE `MotorPosition`;
+
+ALTER TABLE `Image` DROP `motorPositionId`;
+ALTER TABLE pydb.BLSubSample DROP COLUMN motorPositionId;
+
+SET foreign_key_checks = 1;
+
+
