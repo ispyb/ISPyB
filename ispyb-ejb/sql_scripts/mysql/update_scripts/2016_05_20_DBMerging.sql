2826d91a85faf640f81f1b4009eb8eb7b2f50224
Solange Delageniere <solange.delageniere@esrf.fr>
2016-05-27 09:24:22 +0000

    DB merging with DLS
    svn path=/trunk/ispyb-parent/; revision=531

--- ispyb-ejb/sql_scripts/mysql/update_scripts/2016_05_20_DBMerging.sql
+++ ispyb-ejb/sql_scripts/mysql/update_scripts/2016_05_20_DBMerging.sql
@@ -6,6 +6,8 @@ SET foreign_key_checks = 0;
 
 DROP TABLE `WorkflowMesh`;
 DROP TABLE `MotorPosition`;
+ALTER TABLE `DataCollection` DROP FOREIGN KEY `DataCollection_ibfk_6`;
+ALTER TABLE `DataCollection` DROP FOREIGN KEY `DataCollection_ibfk_7`;
 
 SET foreign_key_checks = 1;
 
