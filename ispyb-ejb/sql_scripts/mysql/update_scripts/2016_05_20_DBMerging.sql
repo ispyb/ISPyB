99ad3155d9d494b45c1b10f216b14a423c2e9269
Solange Delageniere <solange.delageniere@esrf.fr>
2016-06-03 13:33:29 +0000

    REVERT to older version because WorkflowMesh IS USED !!!
    svn path=/trunk/ispyb-parent/; revision=541

--- ispyb-ejb/sql_scripts/mysql/update_scripts/2016_05_20_DBMerging.sql
+++ ispyb-ejb/sql_scripts/mysql/update_scripts/2016_05_20_DBMerging.sql
@@ -8,7 +8,7 @@ SET foreign_key_checks = 0;
 --DROP TABLE `MotorPosition`;
 --ALTER TABLE `DataCollection` DROP FOREIGN KEY `DataCollection_ibfk_6`;
 --ALTER TABLE `DataCollection` DROP FOREIGN KEY `DataCollection_ibfk_7`;
-ALTER TABLE `GridInfo` DROP FOREIGN KEY `GridInfo_ibfk_1`;
+--ALTER TABLE `GridInfo` DROP FOREIGN KEY `GridInfo_ibfk_1`;
 
 SET foreign_key_checks = 1;
 
