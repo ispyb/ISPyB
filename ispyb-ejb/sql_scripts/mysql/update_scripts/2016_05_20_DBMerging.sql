ae825c7174a962ac90f0f594b707ea1a559ea7cd
Solange Delageniere <solange.delageniere@esrf.fr>
2016-06-03 13:10:01 +0000

    !!! DO NOT remove WorkflowMesh now !!!
    svn path=/trunk/ispyb-parent/; revision=539

--- ispyb-ejb/sql_scripts/mysql/update_scripts/2016_05_20_DBMerging.sql
+++ ispyb-ejb/sql_scripts/mysql/update_scripts/2016_05_20_DBMerging.sql
@@ -4,10 +4,10 @@ alter table Person drop passwd;
 
 SET foreign_key_checks = 0;
 
-DROP TABLE `WorkflowMesh`;
-DROP TABLE `MotorPosition`;
-ALTER TABLE `DataCollection` DROP FOREIGN KEY `DataCollection_ibfk_6`;
-ALTER TABLE `DataCollection` DROP FOREIGN KEY `DataCollection_ibfk_7`;
+--DROP TABLE `WorkflowMesh`;
+--DROP TABLE `MotorPosition`;
+--ALTER TABLE `DataCollection` DROP FOREIGN KEY `DataCollection_ibfk_6`;
+--ALTER TABLE `DataCollection` DROP FOREIGN KEY `DataCollection_ibfk_7`;
 ALTER TABLE `GridInfo` DROP FOREIGN KEY `GridInfo_ibfk_1`;
 
 SET foreign_key_checks = 1;
