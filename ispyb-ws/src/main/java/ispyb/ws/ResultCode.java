ce76de370c445c3392432271dcccba94f62ee2d8
Davy de Durpel <davy.de.durpel@isencia.be>
2016-09-28 05:23:55 +0000

    Added basic support for returning a meaningful result code and message
    svn path=/trunk/ispyb-parent/; revision=696

---
+++ ispyb-ws/src/main/java/ispyb/ws/ResultCode.java
@@ -0,0 +1,53 @@
+package ispyb.ws;
+
+import java.text.MessageFormat;
+
+/**
+ * A list of possible result codes that could be returned to the client application
+ * 
+ * @author durdav
+ *
+ */
+public enum ResultCode {
+	/**
+	 * Parameters:
+	 */
+	OK("ISPYB-0000", "Ok"), 
+	/**
+	 * Parameters: Exception Message
+	 */
+	UNKNOWN_ERROR("ISPYB-0001", "Unknown Error: {0}"), 
+	/**
+	 * Parameters: Proposal Code, Proposal Number
+	 */
+	UNKNOWN_PROPOSAL("ISPYB-0002", "Unknown Proposal: {0}{1}"),
+	/**
+	 * Parameters: Experiment ID
+	 */
+	UNKNOWN_EXPERIMENT("ISPYB-0003", "Unknown Experiment: {0}"),
+	/**
+	 * Parameters: Exception Message
+	 */
+	RUNTIME_ERROR("ISPYB-0004", "{0}"),
+	/**
+	 * Parameters: Proposal Code, Proposal Number
+	 */
+	NO_EXPERIMENTS_FOUND("ISPYB-0004", "No experiments found for proposal: {0}{1}")
+	;
+	
+	private String code;
+	private String description;
+	
+	private ResultCode(String code, String description) {
+		this.code = code;
+		this.description = description;
+	}
+	
+	public String getCode() {
+		return code;
+	}
+	
+	public String getDescription(Object... args) {
+		return MessageFormat.format(description, args);
+	}
+}
