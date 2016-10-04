265f3fbb5919de09695ab7071e368886efe85186
Solange Delageniere <solange.delageniere@esrf.fr>
2016-10-04 07:48:15 +0000

    svn path=/trunk/ispyb-parent/; revision=701

--- ispyb-ejb/sql_scripts/mysql/update_scripts/2016_09_30_dropForeignKeys.sql
+++ ispyb-ejb/sql_scripts/mysql/update_scripts/2016_09_30_dropForeignKeys.sql
@@ -4,3 +4,14 @@
 USE `pydb`;
 ALTER TABLE `DiffractionPlan` DROP FOREIGN KEY `DiffractionPlan_ibfk_1` ;
 ALTER TABLE `DiffractionPlan` DROP INDEX `DiffractionPlan_FKIndex2` ;
+
+ALTER TABLE `BLSubSample` DROP FOREIGN KEY `BLSubSample_motorPositionfk_1` ;
+ALTER TABLE BLSubSample DROP INDEX BLSubSample_FKIndex4;
+ALTER TABLE `BLSubSample` DROP `motorPositionId`;
+
+ALTER TABLE Image DROP FOREIGN KEY `Image_ibfk_4` ;
+
+ALTER TABLE DataCollection DROP FOREIGN KEY `DataCollection_ibfk_6` ;
+ALTER TABLE DataCollection DROP FOREIGN KEY `DataCollection_ibfk_7` ;
+
+ALTER TABLE WorkflowMesh DROP FOREIGN KEY `WorkflowMesh_ibfk_1` ;
