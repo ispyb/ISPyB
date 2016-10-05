f624700fe4b86d4572a6fa976a0d17a3b6e5c160
Alejandro de Maria <demariaa@esrf.fr>
2016-10-05 06:55:46 +0000

    Organized imports
    svn path=/trunk/ispyb-parent/; revision=708

--- ispyb-ws/src/main/java/ispyb/ws/rest/saxs/ModelingRestWebService.java
+++ ispyb-ws/src/main/java/ispyb/ws/rest/saxs/ModelingRestWebService.java
@@ -1,7 +1,6 @@
 package ispyb.ws.rest.saxs;
 
 import ispyb.server.biosaxs.services.utils.reader.pdb.PDBFactoryProducer;
-import ispyb.ws.rest.RestWebService;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
