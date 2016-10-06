ef4aa22529163a6b7dbcedf1c9de89ac5d0bf1d6
Solange Delageniere <solange.delageniere@esrf.fr>
2016-10-06 13:20:23 +0000

    svn path=/trunk/ispyb-parent/; revision=723

--- ispyb-ejb/sql_scripts/mysql/update_scripts/2016_10_05_Add_index_experiment_to_session.sql
+++ ispyb-ejb/sql_scripts/mysql/update_scripts/2016_10_05_Add_index_experiment_to_session.sql
@@ -1,9 +1,10 @@
-ALTER TABLE `pydb`.`Experiment` 
+USE `pydb`;
+
+ALTER TABLE Experiment 
 CHANGE COLUMN `sessionId` `sessionId` INT(10) UNSIGNED NULL DEFAULT NULL ,
 ADD INDEX `fk_Experiment_To_session_idx` (`sessionId` ASC);
-ALTER TABLE `pydb`.`Experiment` 
+
+ALTER TABLE Experiment 
 ADD CONSTRAINT `fk_Experiment_To_session`
   FOREIGN KEY (`sessionId`)
-  REFERENCES `pydb`.`BLSession` (`sessionId`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
+  REFERENCES BLSession (`sessionId`) ON DELETE NO ACTION ON UPDATE NO ACTION;
