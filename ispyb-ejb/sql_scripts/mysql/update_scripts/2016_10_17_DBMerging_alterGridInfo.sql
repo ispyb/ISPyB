d5dbbc812ea36e7e7be60853ca7d6d9aa38d3448
Solange Delageniere <solange.delageniere@esrf.fr>
2016-10-17 12:49:15 +0000

    DBMerging : alter GridInfo --> EJBs to be updated
    svn path=/trunk/ispyb-parent/; revision=739

---
+++ ispyb-ejb/sql_scripts/mysql/update_scripts/2016_10_17_DBMerging_alterGridInfo.sql
@@ -0,0 +1,14 @@
+
+-- 
+
+USE `pydb`;
+
+ALTER TABLE `GridInfo` 
+  MODIFY `workflowMeshId` int(11) unsigned DEFAULT NULL,
+  ADD `orientation` enum('vertical','horizontal') DEFAULT 'horizontal',
+  ADD `dataCollectionGroupId` int(11) DEFAULT NULL,
+  ADD `pixelspermicronX` float DEFAULT NULL,
+  ADD `pixelspermicronY` float DEFAULT NULL,
+  ADD `snapshot_offsetxpixel` float DEFAULT NULL,
+  ADD `snapshot_offsetypixel` float DEFAULT NULL,
+  ADD CONSTRAINT `GridInfo_ibfk_2` FOREIGN KEY (`dataCollectionGroupId`) REFERENCES `DataCollectionGroup` (`dataCollectionGroupId`) ON DELETE NO ACTION ON UPDATE NO ACTION ;;
