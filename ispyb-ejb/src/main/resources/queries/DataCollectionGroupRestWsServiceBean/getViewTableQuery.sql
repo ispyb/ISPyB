b29a3d405a15790c083eafd27044d0a8af56525c
Alejandro de Maria <demariaa@esrf.fr>
2016-10-25 10:06:16 +0000

    Remove prefix pydb from the queries
    svn path=/trunk/ispyb-parent/; revision=757

---
+++ ispyb-ejb/src/main/resources/queries/DataCollectionGroupRestWsServiceBean/getViewTableQuery.sql
@@ -0,0 +1,52 @@
+select *, 
+(select GROUP_CONCAT(workflowStepId) from WorkflowStep where WorkflowStep.workflowId = v_datacollection_summary.Workflow_workflowId order by  WorkflowStep.workflowStepId DESC) as WorkflowStep_workflowStepId,
+(select GROUP_CONCAT(workflowStepType) from WorkflowStep where WorkflowStep.workflowId = v_datacollection_summary.Workflow_workflowId) as WorkflowStep_workflowStepType,
+(select GROUP_CONCAT(status) from WorkflowStep where WorkflowStep.workflowId = v_datacollection_summary.Workflow_workflowId) as WorkflowStep_status,		
+				
+GROUP_CONCAT(`AutoProcProgram_processingPrograms` SEPARATOR ', ') AS `processingPrograms`,
+GROUP_CONCAT(`AutoProcProgram_processingStatus` SEPARATOR ', ') AS `processingStatus`,
+GROUP_CONCAT(`AutoProcIntegration_autoProcIntegrationId` SEPARATOR ', ') AS `autoProcIntegrationId`, 
+
+GROUP_CONCAT(`cell_a` SEPARATOR ', ') AS `Autoprocessing_cell_a`, 
+GROUP_CONCAT(`cell_b` SEPARATOR ', ') AS `Autoprocessing_cell_b`, 
+GROUP_CONCAT(`cell_c` SEPARATOR ', ') AS `Autoprocessing_cell_c`, 
+GROUP_CONCAT(`cell_alpha` SEPARATOR ', ') AS `Autoprocessing_cell_alpha`, 
+GROUP_CONCAT(`cell_beta` SEPARATOR ', ') AS `Autoprocessing_cell_beta`, 
+GROUP_CONCAT(`cell_gamma` SEPARATOR ', ') AS `Autoprocessing_cell_gamma`, 
+
+GROUP_CONCAT(`autoProcId` SEPARATOR ', ') AS `autoProcIds`,
+GROUP_CONCAT(`scalingStatisticsType` SEPARATOR ', ') AS `scalingStatisticsTypes`, 
+GROUP_CONCAT(`resolutionLimitHigh` SEPARATOR ', ') AS `resolutionsLimitHigh`,  
+GROUP_CONCAT(`resolutionLimitLow` SEPARATOR ', ') AS `resolutionsLimitLow`, 
+GROUP_CONCAT(`rMerge` SEPARATOR ', ') AS `rMerges`,
+GROUP_CONCAT(`completeness` SEPARATOR ', ') AS `completenessList`,  
+GROUP_CONCAT(`AutoProc_spaceGroup` SEPARATOR ', ') AS `AutoProc_spaceGroups`,
+				
+(SELECT GROUP_CONCAT(distinct(`SpaceGroup`.`spaceGroupShortName`)) AS `v_datacollection_summary_phasing_spaceGroupShortName` 
+	FROM `AutoProcIntegration` 
+	LEFT JOIN `AutoProcScaling_has_Int` ON `AutoProcScaling_has_Int`.`autoProcIntegrationId` = `AutoProcIntegration`.`autoProcIntegrationId`  
+	LEFT JOIN `AutoProcScaling` ON `AutoProcScaling`.`autoProcScalingId` = `AutoProcScaling_has_Int`.`autoProcScalingId` 
+	LEFT JOIN `Phasing_has_Scaling` ON `Phasing_has_Scaling`.`autoProcScalingId` = `AutoProcScaling`.`autoProcScalingId`  
+	LEFT JOIN `PhasingStep` ON `PhasingStep`.`autoProcScalingId` = `Phasing_has_Scaling`.`autoProcScalingId` 
+	LEFT JOIN `SpaceGroup` ON `SpaceGroup`.`spaceGroupId` = `PhasingStep`.`spaceGroupId` 
+	where `AutoProcIntegration`.`dataCollectionId` = v_datacollection_summary.DataCollection_dataCollectionId   
+) as Phasing_spaceGroup,
+
+
+
+(SELECT GROUP_CONCAT(distinct(`PhasingStep`.`phasingStepType`)) AS `v_datacollection_summary_phasing_spaceGroupShortName`
+    FROM `AutoProcIntegration` 
+        LEFT JOIN `AutoProcScaling_has_Int` ON `AutoProcScaling_has_Int`.`autoProcIntegrationId` = `AutoProcIntegration`.`autoProcIntegrationId`  
+        LEFT JOIN `AutoProcScaling` ON `AutoProcScaling`.`autoProcScalingId` = `AutoProcScaling_has_Int`.`autoProcScalingId` 
+        LEFT JOIN `Phasing_has_Scaling` ON `Phasing_has_Scaling`.`autoProcScalingId` = `AutoProcScaling`.`autoProcScalingId`  
+        LEFT JOIN `PhasingStep` ON `PhasingStep`.`autoProcScalingId` = `Phasing_has_Scaling`.`autoProcScalingId` 
+        LEFT JOIN `SpaceGroup` ON `SpaceGroup`.`spaceGroupId` = `PhasingStep`.`spaceGroupId` 
+		where `AutoProcIntegration`.`dataCollectionId` = v_datacollection_summary.DataCollection_dataCollectionId   
+) as Phasing_phasingStepType, 
+				
+				 
+(select SUM(numberOfImages) FROM DataCollection where dataCollectionGroupId = v_datacollection_summary.DataCollectionGroup_dataCollectionGroupId) as totalNumberOfImages,
+(select count(*) FROM DataCollection where dataCollectionGroupId = v_datacollection_summary.DataCollectionGroup_dataCollectionGroupId) as totalNumberOfDataCollections,
+(select MAX(imageId) FROM Image where dataCollectionId = v_datacollection_summary.DataCollection_dataCollectionId) as lastImageId,
+(select MAX(imageId) FROM Image where dataCollectionId = v_datacollection_summary.DataCollection_dataCollectionId) as firstImageId
+from v_datacollection_summary
