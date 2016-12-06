d40b49393c3a2d409b11bbce4ba43d990d4f07dc
Solange Delageniere <solange.delageniere@esrf.fr>
2016-12-06 15:23:20 +0000

    add the first and last line of script to keep track of changes in DB
    svn path=/trunk/ispyb-parent/; revision=844

--- ispyb-ejb/sql_scripts/mysql/update_scripts/2016_12_06_Added_ccAno_sigAno.sql
+++ ispyb-ejb/sql_scripts/mysql/update_scripts/2016_12_06_Added_ccAno_sigAno.sql
@@ -1,3 +1,10 @@
+-- first line of script
+insert into SchemaStatus (scriptName, schemaStatus) values ('2016_12_06_Added_ccAno_sigAno.sql','ONGOING');
+
+
 ALTER TABLE `pydb`.`AutoProcScalingStatistics` 
 ADD COLUMN `ccAno` FLOAT NULL DEFAULT NULL AFTER `ccHalf`,
 ADD COLUMN `sigAno` VARCHAR(45) NULL AFTER `ccAno`;
+
+-- last line of script
+update SchemaStatus set schemaStatus = 'DONE' where scriptName = '2016_12_06_Added_ccAno_sigAno.sql';
