d450558538010429f5bc30dfc0c74713cf1527ee
Alejandro de Maria <demariaa@esrf.fr>
2016-12-06 15:19:58 +0000

    Update views for the webservices
    svn path=/trunk/ispyb-parent/; revision=843

--- ispyb-ejb/src/main/resources/queries/DataCollectionRestWsServiceBean/getViewTableQuery.sql
+++ ispyb-ejb/src/main/resources/queries/DataCollectionRestWsServiceBean/getViewTableQuery.sql
@@ -7,5 +7,25 @@ select
     LEFT JOIN `AutoProcIntegration` ON `AutoProcIntegration`.`autoProcIntegrationId` = `AutoProcScaling_has_Int`.`autoProcIntegrationId` 
 	LEFT JOIN `DataCollection` ON `DataCollection`.`dataCollectionId` = `AutoProcIntegration`.`dataCollectionId` 
 	where `DataCollection`.`dataCollectionId` = v_datacollection.dataCollectionId
-) as hasPhasing
+) as hasPhasing,
+(SELECT GROUP_CONCAT(value1) FROM WorkflowMesh
+	LEFT JOIN `Workflow` ON `Workflow`.`workflowId` = `WorkflowMesh`.`workflowId`   
+    LEFT JOIN `DataCollectionGroup` ON `Workflow`.`workflowId` = `DataCollectionGroup`.`workflowId`   
+	where `DataCollectionGroup`.`dataCollectionGroupId` = v_datacollection.dataCollectionGroupId
+) as value1,
+(SELECT GROUP_CONCAT(value2) FROM WorkflowMesh
+	LEFT JOIN `Workflow` ON `Workflow`.`workflowId` = `WorkflowMesh`.`workflowId`   
+    LEFT JOIN `DataCollectionGroup` ON `Workflow`.`workflowId` = `DataCollectionGroup`.`workflowId`   
+	where `DataCollectionGroup`.`dataCollectionGroupId` = v_datacollection.dataCollectionGroupId
+) as value2,
+(SELECT GROUP_CONCAT(value3) FROM WorkflowMesh
+	LEFT JOIN `Workflow` ON `Workflow`.`workflowId` = `WorkflowMesh`.`workflowId`   
+    LEFT JOIN `DataCollectionGroup` ON `Workflow`.`workflowId` = `DataCollectionGroup`.`workflowId`   
+	where `DataCollectionGroup`.`dataCollectionGroupId` = v_datacollection.dataCollectionGroupId
+) as value3,
+(SELECT GROUP_CONCAT(value4) FROM WorkflowMesh
+	LEFT JOIN `Workflow` ON `Workflow`.`workflowId` = `WorkflowMesh`.`workflowId`   
+    LEFT JOIN `DataCollectionGroup` ON `Workflow`.`workflowId` = `DataCollectionGroup`.`workflowId`   
+	where `DataCollectionGroup`.`dataCollectionGroupId` = v_datacollection.dataCollectionGroupId
+) as value4
  from v_datacollection 
--- ispyb-ejb/src/main/resources/queries/PhasingRestWsServiceBean/getViewTableQuery.sql
+++ ispyb-ejb/src/main/resources/queries/PhasingRestWsServiceBean/getViewTableQuery.sql
@@ -7,12 +7,22 @@ GROUP_CONCAT(`PhasingStatistics_statisticsValue` SEPARATOR ', ') AS `statisticsV
 FROM `PhasingProgramAttachment` 
 WHERE ((`PhasingProgramAttachment`.`phasingProgramRunId` = `PhasingProgramRun_phasingProgramRunId`) AND (`PhasingProgramAttachment`.`fileName` LIKE '%.csv%')) 
 ORDER BY `PhasingProgramAttachment`.`phasingProgramAttachmentId`) AS `csv`,
+
 (SELECT GROUP_CONCAT(PhasingProgramAttachment.phasingProgramAttachmentId)
 FROM `PhasingProgramAttachment` 
 WHERE ((`PhasingProgramAttachment`.`phasingProgramRunId` = `PhasingProgramRun_phasingProgramRunId`) AND (`PhasingProgramAttachment`.`fileName` LIKE '%.map%')) 
 ORDER BY `PhasingProgramAttachment`.`phasingProgramAttachmentId`) AS `map` , 
+
 (SELECT GROUP_CONCAT(PhasingProgramAttachment.phasingProgramAttachmentId)
 FROM `PhasingProgramAttachment` 
 WHERE ((`PhasingProgramAttachment`.`phasingProgramRunId` = `PhasingProgramRun_phasingProgramRunId`) AND (`PhasingProgramAttachment`.`fileName` LIKE '%.pdb%')) 
-ORDER BY `PhasingProgramAttachment`.`phasingProgramAttachmentId`) AS `pdb`
+ORDER BY `PhasingProgramAttachment`.`phasingProgramAttachmentId`) AS `pdb`,
+
+
+(SELECT GROUP_CONCAT(PhasingProgramAttachment.phasingProgramAttachmentId)
+FROM `PhasingProgramAttachment` 
+WHERE ((`PhasingProgramAttachment`.`phasingProgramRunId` = `PhasingProgramRun_phasingProgramRunId`) AND (`PhasingProgramAttachment`.`fileName` LIKE '%.png%')) 
+ORDER BY `PhasingProgramAttachment`.`phasingProgramAttachmentId`) AS `png`
+
+
 from v_phasing 
