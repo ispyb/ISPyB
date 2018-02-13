71e75362bfee00df5b3ab80646152a617303bf03
Karl Levik <karl.levik@diamond.ac.uk>
2018-02-13 15:05:34 +0000

    Correct script name in insert+update statements

--- ispyb-ejb/db/scripts/ahead/2018_02_13_BLSample_subLocation.sql
+++ ispyb-ejb/db/scripts/ahead/2018_02_13_BLSample_subLocation.sql
@@ -1,7 +1,7 @@
 USE pydb;
 
-INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('20180213_BLSample_subLocation.sql', 'ONGOING');
+INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2018_02_13_BLSample_subLocation.sql', 'ONGOING');
 
 ALTER TABLE BLSample ADD subLocation SMALLINT UNSIGNED COMMENT 'Indicates the sample\'s location on a multi-sample pin, where 1 is closest to the pin base';
 
-UPDATE SchemaStatus SET schemaStatus = 'DONE' where scriptName = '20180213_BLSample_subLocation.sql';
+UPDATE SchemaStatus SET schemaStatus = 'DONE' where scriptName = '2018_02_13_BLSample_subLocation.sql';
