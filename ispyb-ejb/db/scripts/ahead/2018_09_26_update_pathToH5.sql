7b56ba36df8c90de5eb3cade4ec5c7275d4224dc
DimaMolod <d.s.molodenskiy@ya.ru>
2018-09-26 14:26:03 +0200

    Added missing field "pathToH5" to the Measurment table

---
+++ ispyb-ejb/db/scripts/ahead/2018_09_26_update_pathToH5.sql
@@ -0,0 +1,6 @@
+INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2018_09_26_update_pathToH5.sql','ONGOING');
+
+USE pydb;
+ALTER TABLE Measurement ADD pathToH5 VARCHAR(512);
+
+update SchemaStatus set schemaStatus = 'DONE' where scriptName = '2018_09_26_update_pathToH5.sql';
