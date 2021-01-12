022cb8926b2ed168764a1f5d6b311786adc3c26d
delageniere <delageni@esrf.fr>
2021-01-12 11:00:43 +0100

    change collation for Person.phoneNumber

---
+++ ispyb-ejb/db/scripts/ahead/2021_01_12_Person_newCollation.sql
@@ -0,0 +1,5 @@
+INSERT IGNORE INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2021_01_12_Person_new_collation.sql', 'ONGOING');
+
+ALTER TABLE `Person` CHANGE `phoneNumber` `phoneNumber` VARCHAR(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;
+
+UPDATE SchemaStatus SET schemaStatus = 'DONE' WHERE scriptName = '2021_01_12_Person_new_collation.sql';
