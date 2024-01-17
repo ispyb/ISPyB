cb0ef8a0563309578dcdb2b22be78af85e05f263
Marjolaine Bodin <bodin@esrf.fr>
2024-01-17 14:41:49 +0100

    add workflow types

---
+++ ispyb-ejb/db/scripts/ahead/2024_01_17_add_workflow_type.sql
@@ -0,0 +1,10 @@
+use pydb;
+
+INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2024_01_17_add_workflow_type.sql', 'ONGOING');
+
+
+ALTER TABLE `pydb`.`Workflow`
+CHANGE COLUMN `workflowType` `workflowType` ENUM('Characterisation','Undefined','BioSAXS Post Processing','EnhancedCharacterisation','LineScan','MeshScan','Dehydration','KappaReorientation','BurnStrategy','XrayCentering','DiffractionTomography','TroubleShooting','VisualReorientation','HelicalCharacterisation','GroupedProcessing','MXPressE','MXPressO','MXPressL','MXScore','MXPressI','MXPressM','MXPressA','CollectAndSpectra','LowDoseDC','EnergyInterleavedMAD','MXPressF','MXPressH','MXPressP','MXPressP_SAD','MXPressR','MXPressR_180','MXPressR_dehydration','MeshAndCollect','MeshAndCollectFromFile',  'CalibrateKappa', 'CenterRotationAxis', 'CentreBeam', 'CentrePin', 'ChipX_collect', 'ChipX_collect_sections', 'ChipX_left_position', 'ChipX_right_position', 'DistanceCalibration', 'IceRingsMeshScan', 'MeshAndBurn', 'MXPressA_globalp', 'MXPressA_globalp_full', 'MXPressA_globalp_minimal', 'MXPressA_globalp_quick', 'MXPressA_HTX', 'MXPressF_twoMeshes', 'MXPressK', 'MXPressO_540', 'MXPressS', 'TroubleShootingWithDialog'
+) NULL DEFAULT NULL ; 
+
+UPDATE SchemaStatus SET schemaStatus = 'DONE' WHERE scriptName = '2024_01_17_add_workflow_typed.sql';
