72dcbae779010abbc6c3b1a0dad33b218e1c3dc0
Alejandro De Maria Antolinos <demariaa@esrf.fr>
2024-09-09 15:05:38 +0200

    Added more fields for shipment

--- ispyb-ejb/src/main/java/ispyb/server/mx/vos/sample/SampleInfo.java
+++ ispyb-ejb/src/main/java/ispyb/server/mx/vos/sample/SampleInfo.java
@@ -21,14 +21,16 @@ package ispyb.server.mx.vos.sample;
 import java.io.Serializable;
 
 /**
- * Sample info needed for mxCuBE to display the list of samples (on a beamline, with the 'processing' status)
+ * Sample info needed for mxCuBE to display the list of samples (on a beamline,
+ * with the 'processing' status)
+ * 
  * @author BODIN
  *
  */
-public class SampleInfo implements Serializable, Cloneable{
+public class SampleInfo implements Serializable, Cloneable {
 
 	private static final long serialVersionUID = -3203393739645665888L;
-	
+
 	// sample info
 	protected Integer sampleId;
 	protected String sampleName;
@@ -36,10 +38,10 @@ public class SampleInfo implements Serializable, Cloneable{
 	protected Double holderLength;
 	protected String sampleLocation;
 	protected String smiles;
-	
+
 	// protein
 	protected String proteinAcronym;
-	
+
 	// crystal
 	protected Integer crystalId;
 	protected String crystalSpaceGroup;
@@ -49,28 +51,38 @@ public class SampleInfo implements Serializable, Cloneable{
 	protected Double cellAlpha;
 	protected Double cellBeta;
 	protected Double cellGamma;
-	
+
 	// diffractionPlan
 	protected Double minimalResolution;
 	protected String experimentType;
 	protected DiffractionPlanWS3VO diffractionPlan;
-	
+
 	// container
 	protected String containerSampleChangerLocation;
 	protected String containerCode;
-	
+
 	// imagepath
 	protected String blsampleImagePath;
-	
+
+	protected Integer dewarId;
+	protected String containerComments;
+	protected String dewarCode;
+	protected String shippingName;
+	protected String shippingComments;
+	protected Integer containerCapacity;
+	protected String containerType;
+	protected Integer shippingId;
+
 	public SampleInfo() {
 		super();
 	}
 
 	public SampleInfo(Integer sampleId, String sampleName, String code,
 			Double holderLength, String sampleLocation, String smiles, String proteinAcronym, Integer crystalId,
-			String crystalSpaceGroup, Double cellA, Double cellB, Double cellC, Double cellAlpha, 
+			String crystalSpaceGroup, Double cellA, Double cellB, Double cellC, Double cellAlpha,
 			Double cellBeta, Double cellGamma, Double minimalResolution, String experimentType,
-			String containerSampleChangerLocation, String containerCode, DiffractionPlanWS3VO diffractionPlan, String blsampleImagePath) {
+			String containerSampleChangerLocation, String containerCode, DiffractionPlanWS3VO diffractionPlan,
+			String blsampleImagePath) {
 		super();
 		this.sampleId = sampleId;
 		this.sampleName = sampleName;
@@ -80,7 +92,7 @@ public class SampleInfo implements Serializable, Cloneable{
 		this.containerCode = containerCode;
 		this.smiles = smiles;
 		this.proteinAcronym = proteinAcronym;
-		this.crystalId=crystalId;
+		this.crystalId = crystalId;
 		this.crystalSpaceGroup = crystalSpaceGroup;
 		this.cellA = cellA;
 		this.cellB = cellB;
@@ -88,13 +100,97 @@ public class SampleInfo implements Serializable, Cloneable{
 		this.cellAlpha = cellAlpha;
 		this.cellBeta = cellBeta;
 		this.cellGamma = cellGamma;
-		this.diffractionPlan = diffractionPlan ;
+		this.diffractionPlan = diffractionPlan;
 		this.minimalResolution = minimalResolution;
 		this.experimentType = experimentType;
 		this.containerSampleChangerLocation = containerSampleChangerLocation;
 		this.blsampleImagePath = blsampleImagePath;
 	}
-	
+
+	public SampleInfo(Integer sampleId, String sampleName, String code,
+			Double holderLength, String sampleLocation, String smiles, String proteinAcronym, Integer crystalId,
+			String crystalSpaceGroup, Double cellA, Double cellB, Double cellC, Double cellAlpha,
+			Double cellBeta, Double cellGamma, Double minimalResolution, String experimentType,
+			String containerSampleChangerLocation, String containerCode, DiffractionPlanWS3VO diffractionPlan,
+			String blsampleImagePath,
+			Integer dewarId, String containerComments, String dewarCode, String shippingName,
+			String shippingComments, Integer containerCapacity, String containerType, Integer shippingId) {
+		super();
+		this.sampleId = sampleId;
+		this.sampleName = sampleName;
+		this.code = code;
+		this.holderLength = holderLength;
+		this.sampleLocation = sampleLocation;
+		this.containerCode = containerCode;
+		this.smiles = smiles;
+		this.proteinAcronym = proteinAcronym;
+		this.crystalId = crystalId;
+		this.crystalSpaceGroup = crystalSpaceGroup;
+		this.cellA = cellA;
+		this.cellB = cellB;
+		this.cellC = cellC;
+		this.cellAlpha = cellAlpha;
+		this.cellBeta = cellBeta;
+		this.cellGamma = cellGamma;
+		this.diffractionPlan = diffractionPlan;
+		this.minimalResolution = minimalResolution;
+		this.experimentType = experimentType;
+		this.containerSampleChangerLocation = containerSampleChangerLocation;
+		this.blsampleImagePath = blsampleImagePath;
+		this.dewarId = dewarId;
+		this.containerComments = containerComments;
+		this.dewarCode = dewarCode;
+		this.shippingName = shippingName;
+		this.shippingComments = shippingComments;
+		this.containerCapacity = containerCapacity;
+		this.containerType = containerType;
+		this.shippingId = shippingId;
+	}
+
+	public void setDewarId(Integer dewarId) {
+		this.dewarId = dewarId;
+	}
+
+	public void setContainerComments(String containerComments) {
+		this.containerComments = containerComments;
+	}
+
+	public void setDewarCode(String dewarCode) {
+		this.dewarCode = dewarCode;
+	}
+
+	public void setShippingName(String shippingName) {
+		this.shippingName = shippingName;
+	}
+
+	public void setShippingComments(String shippingComments) {
+		this.shippingComments = shippingComments;
+	}
+
+	public Integer getDewarId() {
+		return dewarId;
+	}
+
+	public String getContainerComments() {
+		return containerComments;
+	}
+
+	public String getDewarCode() {
+		return dewarCode;
+	}
+
+	public String getShippingName() {
+		return shippingName;
+	}
+
+	public String getShippingComments() {
+		return shippingComments;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
 	public Integer getSampleId() {
 		return sampleId;
 	}
@@ -262,6 +358,30 @@ public class SampleInfo implements Serializable, Cloneable{
 
 	public void setBlsampleImagePath(String blsampleImagePath) {
 		this.blsampleImagePath = blsampleImagePath;
-	}	
+	}
+
+	public void setShippingId(Integer shippingId) {
+		this.shippingId = shippingId;
+	}
+
+	public Integer getShippingId() {
+		return shippingId;
+	}
+
+	public void setContainerCapacity(Integer containerCapacity) {
+		this.containerCapacity = containerCapacity;
+	}
+
+	public void setContainerType(String containerType) {
+		this.containerType = containerType;
+	}
+
+	public Integer getContainerCapacity() {
+		return containerCapacity;
+	}
+
+	public String getContainerType() {
+		return containerType;
+	}
 
 }
